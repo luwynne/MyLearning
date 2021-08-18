02c7c8e09d017141c905154afa16adba0904028d
Luiz <luwynne@aol.co.uk>
2021-08-18 10:46:25 -0300

    First commit

---
+++ client/pages/index.js
@@ -0,0 +1,12 @@
+const Index = () => {
+    return (
+        <>
+            <div className="position-relative">
+                <h1 className="jumbotron text-center bg-primary square">Hello world</h1>
+                <p>from Next.js</p>
+            </div>
+        </>
+    )
+}
+
+export default Index;
